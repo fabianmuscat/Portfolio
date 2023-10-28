c8632ce339db645bfd00102b978e67e7e8ed2fd6
Fabian Muscat <fabian.muscat@icloud.com>
2023-10-28 21:47:27 +0200

    Added flowbite and set default colours

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+            <h1
+                class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                Fabian Muscat
+            </h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro corporis adipisci rerum deleniti. Ab eveniet
+                quo excepturi officia esse eius pariatur? Expedita accusantium nesciunt, laborum quisquam suscipit est nulla
+                reiciendis.
+                Rerum, laudantium aliquid? Quis neque consequatur obcaecati fugiat aliquid corporis hic, veritatis ducimus
+                nesciunt quidem eligendi voluptate? Nostrum quidem voluptatum, quae veritatis saepe reiciendis non corporis
+                eaque, provident rerum repellendus.
+            </p>
+        </div>
+    </section>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
